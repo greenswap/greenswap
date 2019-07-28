c6b7597ca971f757a98fc7cf25c0a175b629c230
Zuzana Jeschke <zuz@zuzjes.com>
2019-07-28 21:47:27 +0200

    dd somethink

--- pages/index.js
+++ pages/index.js
@@ -81,18 +81,18 @@ const Navigation = props => (
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
             <a className="nav-link" href="#">
-              Domů <span className="sr-only">(current)</span>
+              DOMŮ <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">
-              Tipy/Triky
+              TIPY/TRIKY
             </a>
           </li>
 
           <li className="nav-item">
             <a className="nav-link" href="#">
-              Můj profil
+              PROFIL
             </a>
           </li>
         </ul>
