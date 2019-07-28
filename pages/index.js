2fee654a3228f81a4f46664cbb2715bfcd1782e2
Zuzana Jeschke <zuz@zuzjes.com>
2019-07-28 21:04:18 +0200

    add navbar

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,25 @@ import _JSXStyle from "styled-jsx/style";
 import { card, Container, Col, Row, Button } from "react-bootstrap";
 import bgImg from "../img/monstera.jpg";
 import monsteraSq from "../img/monstera-sq.jpg";
-
-const ContainerTight = props => (
-  <div className="ContainerTight">
-    {props.children}
-    <style jsx>{`
-      .ContainerTight {
-        width: 100%;
-        padding-right: 15px;
-        padding-left: 15px;
-        margin-right: auto;
-        margin-left: auto;
-        max-width: 1000px;
-      }
-    `}</style>
-  </div>
-);
+import hoyaSq from "../img/hoya-sq.jpg";
 
 const TopBar = props => (
   <div className="TopBar">
-    <h1 style={{ color: "#335808" }}>GreenSwap</h1>
     <div className="TopBar-img">
       <div className="SearchBar">
         <div className="input-group pt-5 pb-5 pl-3 pr-3">
-          <input type="text" className="form-control" aria-label="" placeholder="Jakou rostlinu hledáte ?"/>
-          <input type="text" className="form-control" aria-label="location" placeholder="location"/>
+          <input
+            type="text"
+            className="form-control"
+            aria-label=""
+            placeholder="Jakou rostlinu hledáte ?"
+          />
+          <input
+            type="text"
+            className="form-control"
+            aria-label="location"
+            placeholder="location"
+          />
           <div className="input-group-append">
             <button className="btn btn-outline-secondary" type="button">
               Button
@@ -46,13 +40,13 @@ const TopBar = props => (
 const Flower = props => (
   <div className="col-4">
     <div className="card">
-      <div style={{height: '200px', backgroundImage: `url(${monsteraSq})`, backgroundSize: 'cover'}}>
-        {/* <img
-          className="img-fluid"
-          src={bgImg}
-          alt=""
-        /> */}
-      </div>
+      <div
+        style={{
+          height: "200px",
+          backgroundImage: `url(${props.flower.image})`,
+          backgroundSize: "cover"
+        }}
+      />
       <div className="card-body">
         <h5 className="card-title">{props.flower.name}</h5>
         <a href="#" className="btn btn-primary mr-1">
@@ -64,30 +58,47 @@ const Flower = props => (
       </div>
     </div>
   </div>
+);
+const Navigation = props => (
+  <div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        <h1 style={{ color: "#335808" }}>GreenSwap</h1>
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
 
-  // <div className="square col-3 m-4">
-  //   <div className="Flower-text">
-  //     <Row>
-  //       <Col><h3>Kytka Krana</h3></Col>
-  //       <Col><Button>Free</Button></Col>
-  //     </Row>
-  //   </div>
-  //   <style jsx>{`
-  //     .square {
-  //       flex: 1 0 calc(25% - 10px);
-  //       background-image: url(${monsteraSq});
-  //     }
-  //     .square:after{
-  //       content: '';
-  //       display: block;
-  //       padding-bottom: 100%;
-  //     }
-  //     .Flower-text {
-  //       background: green;
-  //       bottom: 0;
-  //     }
-  //   `}</style>
-  // </div>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Domů <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Tipy/Triky
+            </a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Můj profil
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 );
 
 const FlowerOffer = props => (
@@ -95,8 +106,8 @@ const FlowerOffer = props => (
     {props.children}
     <div className="d-flex row wrap">
       {[
-        { image: "../img/monstera-sq.jpg", name: "Monstera", price: "300kc" },
-        { name: "Voskovka", price: "Free" }
+        { image: monsteraSq, name: "Monstera", price: "300kc" },
+        { image: hoyaSq, name: "Voskovka", price: "Free" }
       ].map((flower, i) => {
         return <Flower key={i} flower={flower} />;
       })}
@@ -112,6 +123,7 @@ const FlowerOffer = props => (
 function Home() {
   return (
     <Container>
+      <Navigation/>
       <TopBar />
       <FlowerOffer />
     </Container>
